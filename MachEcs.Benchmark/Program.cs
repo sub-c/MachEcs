faa8467489f076a8afd79297b156325928f6b115
Sub-C <halolockdown@yahoo.com>
2021-08-06 19:30:10 -0700

    added benchmark project

---
+++ MachEcs.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace MachEcs.Benchmark
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BitArrayBenchmarks>();
+        }
+    }
+}
