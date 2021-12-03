1bf36452bd2f991307c3b64d5c44dc9240e8a76e
sub-c <79296426+sub-c@users.noreply.github.com>
2021-12-02 19:50:05 -0500

    added initial event addon implementation

---
+++ MachEcs.Events/AgentCache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubC.MachEcs.Events
+{
+    internal sealed class AgentCache
+    {
+        public IDictionary<Type, IMachEventCache> EventCaches { get; } = new Dictionary<Type, IMachEventCache>();
+    }
+}
---
+++ MachEcs.Events/MachEventCache.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SubC.MachEcs.Events
+{
+    internal sealed class MachEventCache<T> : IMachEventCache
+    {
+        public Action<T> EventListeners { get; set; }
+    }
+}
